9780a5c467e7423de6f82c4890f55eb14d87551f
martapla <martaplanella@hotmail.com>
2024-03-27 19:12:20 +0100

    country list

--- src/components/CountriesList.jsx
+++ src/components/CountriesList.jsx
@@ -1,8 +1,41 @@
 import React from 'react'
+import { useState, useEffect } from "react";
+import { Link } from 'react-router-dom';
+
+const ApiUrl = "https://restcountries.com/v3.1/all?fields=name";
+
 
 const CountriesList = () => {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+        const response = await fetch(ApiUrl)
+        const data = await response.json()
+        setCountries(data);
+   
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <h1 className="flex justify-center text-2xl font-bold mt-20">CountriesList</h1>
+
+    <div>
+        <h2 className="flex justify-center text-2xl font-bold my-20">Countries List</h2>
+        
+        <div className="grid grid-cols-3 md:grid-cols-4 gap-6 mx-10">
+          {countries.map((country, index) => (
+              <Link key={index} to={`/country/${country.name.common}`}>
+                  <div key={index} className="flex items-center justify-center bg-red-100 p-2 border-grey-600 rounded-md shadow-md">
+                    {country.name.common}
+                  </div>
+              </Link>
+          ))}
+        </div>
+    </div>
+
+   
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import CountriesList from './components/CountriesList';
 import CountryCard from './components/CountryCard';
@@ -12,7 +11,7 @@ root.render(
     <BrowserRouter>
       <Routes>
         <Route path='/' element={ <CountriesList /> } />
-        <Route path='/country' element={ <CountryCard /> } />
+        <Route path='/country/:id' element={ <CountryCard /> } />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>

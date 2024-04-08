fa6ea35161127e1e2f1116800b95b9451d1dc81b
martapla <martaplanella@hotmail.com>
2024-04-08 12:39:25 +0200

    vercel bg image

--- src/components/RegionButtons.jsx
+++ src/components/RegionButtons.jsx
@@ -5,7 +5,7 @@ const RegionButtons = ({ onRegionChange }) => {
   const regions = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania','All'];
 
   return (
-    <div className='flex flex-wrap justify-center text-md md:text-2xl font-bold gap-6 my-10 md:my-14 mx-6'>
+    <div className='flex flex-wrap justify-center text-lg md:text-2xl font-bold gap-4 md:gap-6 my-8 md:my-14 mx-2 md:mx-6'>
       {regions.map((region, index) => (
           <button
               className=' text-cyan-800 hover:text-red-500 hover:underline'
